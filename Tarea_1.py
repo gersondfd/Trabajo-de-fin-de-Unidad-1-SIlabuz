f39afd784c28ca8c383605a76a7a95d23bf297ca
gersondbg <gersondbg@gmail.com>
2022-11-06 21:45:15 -0500

    opciones del menu

--- Tarea_1.py
+++ Tarea_1.py
@@ -18,3 +18,26 @@ def evaluar_menu(a):
     except ValueError:
         print("\nError. Por favor, ingrese un numero de la lista.\n")
         evaluar_menu(mostrar_menu())
+def seleccionar_menu(a):
+    if a==1:
+        leer_archivo()
+    elif a==2:
+        listar_libros()
+    elif a==3:
+        agregar_libro()
+    elif a==4:
+        eliminar_libro()
+    elif a==5:
+        buscar_isbn_titulo()
+    elif a==6:
+        ordenar_titulo()
+    elif a==7:
+        buscar_autor_editorial_genero()
+    elif a==8:
+        buscar_autores()    
+    elif a==9:
+        editar_libro()
+    elif a==10:
+        guardar_libros()
+    else:
+        print("No disponible")        
