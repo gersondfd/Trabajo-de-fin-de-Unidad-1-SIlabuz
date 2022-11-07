40580fe84839acdb16d39bce68f5456018f5d484
gersondfd <113318318+gersondfd@users.noreply.github.com>
2022-11-06 19:55:35 -0500

    función imprimir

--- Tarea_1.py
+++ Tarea_1.py
@@ -1,2 +1,9 @@
 import csv
 import os
+
+
+
+
+def imprimir_libro(listadelibros,libro):
+    for i in range(0,len(listadelibros[libro])):
+        print(listadelibros[0][i],':',listadelibros[libro][i])
