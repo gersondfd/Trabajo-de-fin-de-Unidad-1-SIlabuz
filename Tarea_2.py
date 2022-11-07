a67672b451b76a7b820c905fdc60a773be49b464
gersondbg <gersondbg@gmail.com>
2022-11-06 23:04:50 -0500

    primer commit

--- Tarea_2.py
+++ Tarea_2.py
@@ -1,1 +1,28 @@
+import os
+import requests
+import json
 
+os.system("clear")
+
+url_pokeapi = 'https://pokeapi.co/api/v2/pokemon/'
+lista_pokemon = ['bulbasaur', 'squirtle', 'charmander', 'abra']
+
+def obtener_pokemon(pokemones):
+    response = requests.get(url_pokeapi + pokemones)
+
+    data = response.json()
+    print(f"Nombre: {data['name']}")
+    print(f"Peso: {data['weight']}")
+
+    lista_habilidades = [habilidad['ability']['name'] for habilidad in data['abilities']]
+
+    print(f"Habilidades: {lista_habilidades}")
+
+    dicc_estadistica = {stat['stat']['name']:stat['base_stat'] for stat in data['stats']}
+
+    print(f"Estadisticas: {dicc_estadistica}")
+
+    print("***********" * 2)
+
+for pokemon in lista_pokemon:
+    obtener_pokemon(pokemon)
