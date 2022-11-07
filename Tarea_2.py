f6d29b333b2d40c5ba784af1f494718020833572
gersondbg <gersondbg@gmail.com>
2022-11-06 23:37:47 -0500

    funcion mostrar habilidades

--- Tarea_2.py
+++ Tarea_2.py
@@ -15,7 +15,16 @@ def pokemon_forma():
     data = data_response.json()
     lista_de_pokemon_forma=[pokemon['name'] for pokemon in data['pokemon_species'] ] 
     print(lista_de_pokemon_forma)
-    
+
+def pokemon_habilidad():
+    url_Pokeapi_Json = 'https://pokeapi.co/api/v2/'
+    url_pokemon = url_Pokeapi_Json+"ability/1/"
+    data_response = requests.get(url_pokemon)
+    data = data_response.json()
+    lista_de_pokemon_habilidad=[pokemon['pokemon']['name'] for pokemon in data['pokemon'] ] 
+    print(lista_de_pokemon_habilidad)
+
 def mostrar_menu():
     a=input('PokeApi\n\nMenú de Opciones\n\n1: Listar pokemons por generación.\n2: Listar pokemons por forma.\n3: Listar pokemons por habilidad.\n4: Listar pokemons por habitat.\n5: Listar pokemons por tipo.\n\nIngrese un número de la lista: ')
 mostrar_menu()
+
