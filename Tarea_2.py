51b77d02cad3d7ed9fbec3c32d82c1a861c42159
ElizabethCuro <elicuro96@gmail.com>
2022-11-06 23:30:48 -0500

    Definiendo la funcion forma de pokeapi

--- Tarea_2.py
+++ Tarea_2.py
@@ -7,6 +7,15 @@ def pokemon_generacion():
     data = data_response.json()
     lista_de_pokemon_generacion=[pokemon['name'] for pokemon in data['pokemon_species'] ] 
     print(lista_de_pokemon_generacion)
+    
+def pokemon_forma():
+    url_Pokeapi_Json = 'https://pokeapi.co/api/v2/'
+    url_pokemon = url_Pokeapi_Json+"/pokemon-shape/6/"
+    data_response = requests.get(url_pokemon)
+    data = data_response.json()
+    lista_de_pokemon_forma=[pokemon['name'] for pokemon in data['pokemon_species'] ] 
+    print(lista_de_pokemon_forma)
+    
 def mostrar_menu():
     a=input('PokeApi\n\nMenú de Opciones\n\n1: Listar pokemons por generación.\n2: Listar pokemons por forma.\n3: Listar pokemons por habilidad.\n4: Listar pokemons por habitat.\n5: Listar pokemons por tipo.\n\nIngrese un número de la lista: ')
 mostrar_menu()
