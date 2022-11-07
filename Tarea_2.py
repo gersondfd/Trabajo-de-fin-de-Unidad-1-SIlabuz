6adfb85772ac3410e6f8181f784a4c126fccdf57
BrySamQL <brysamql@gmail.com>
2022-11-07 00:08:29 -0500

    Enlazar menu con las funciones

--- Tarea_2.py
+++ Tarea_2.py
@@ -7,6 +7,7 @@ def pokemon_generacion():
     data = data_response.json()
     lista_de_pokemon_generacion=[pokemon['name'] for pokemon in data['pokemon_species'] ] 
     print(lista_de_pokemon_generacion)
+    seleccionar_menu(evaluar_menu(mostrar_menu()))
     
 def pokemon_forma():
     url_Pokeapi_Json = 'https://pokeapi.co/api/v2/'
@@ -15,6 +16,7 @@ def pokemon_forma():
     data = data_response.json()
     lista_de_pokemon_forma=[pokemon['name'] for pokemon in data['pokemon_species'] ] 
     print(lista_de_pokemon_forma)
+    seleccionar_menu(evaluar_menu(mostrar_menu()))
 
 def pokemon_habilidad():
     url_Pokeapi_Json = 'https://pokeapi.co/api/v2/'
@@ -23,6 +25,7 @@ def pokemon_habilidad():
     data = data_response.json()
     lista_de_pokemon_habilidad=[pokemon['pokemon']['name'] for pokemon in data['pokemon'] ] 
     print(lista_de_pokemon_habilidad)
+    seleccionar_menu(evaluar_menu(mostrar_menu()))
 
 def pokemon_habitat():
     url_Pokeapi_Json = 'https://pokeapi.co/api/v2/'
@@ -31,6 +34,7 @@ def pokemon_habitat():
     data = data_response.json()
     lista_de_pokemon_habitat=[pokemon['name'] for pokemon in data['pokemon_species']]
     print(lista_de_pokemon_habitat)
+    seleccionar_menu(evaluar_menu(mostrar_menu()))
 
 def pokemon_tipo():
     url_Pokeapi_Json = 'https://pokeapi.co/api/v2/'
@@ -39,6 +43,10 @@ def pokemon_tipo():
     data = data_response.json()
     lista_de_pokemon_tipo=[pokemon['pokemon']['name'] for pokemon in data['pokemon']] 
     print(lista_de_pokemon_tipo)
+    seleccionar_menu(evaluar_menu(mostrar_menu()))
+
+def mostrar_menu():
+     return input('\n\nPokeApi\n\nMenú de Opciones\n\n1: Listar pokemons por generación.\n2: Listar pokemons por forma.\n3: Listar pokemons por habilidad.\n4: Listar pokemons por habitat.\n5: Listar pokemons por tipo.\n\nIngrese un número de la lista: ')
 
 def evaluar_menu(a):
     try:
@@ -51,9 +59,21 @@ def evaluar_menu(a):
             evaluar_menu(mostrar_menu())
     except ValueError:
         print("\nError. Por favor, ingrese un numero de la lista.\n")
-        evaluar_menu(mostrar_menu()) 
+        evaluar_menu(mostrar_menu())
 
-def mostrar_menu(a):
-    a=input('PokeApi\n\nMenú de Opciones\n\n1: Listar pokemons por generación.\n2: Listar pokemons por forma.\n3: Listar pokemons por habilidad.\n4: Listar pokemons por habitat.\n5: Listar pokemons por tipo.\n\nIngrese un número de la lista: ')
-evaluar_menu(mostrar_menu())
+def seleccionar_menu(a):
+    if a==1:
+        pokemon_generacion()
+    elif a==2:
+        pokemon_forma()
+    elif a==3:
+        pokemon_habilidad()
+    elif a==4:
+        pokemon_habitat()
+    elif a==5:
+        pokemon_tipo()
+    else:
+        print("No disponible")
+
+seleccionar_menu(evaluar_menu(mostrar_menu()))
 
